14570b010eee2b979573acab674fbe11fb865537
DESKTOP-DUEGA0S\Admin <r.alen@sapalomera.cat>
2020-12-18 10:10:00 +0100

    hola 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("hola");
         System.out.println("hola2");
         System.out.println("hola3");
     }
