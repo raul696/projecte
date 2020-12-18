5590a71810952ddf5186552e604a471e8066a251
DESKTOP-DUEGA0S\Admin <r.alen@sapalomera.cat>
2020-12-18 09:51:20 +0100

    hola2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner  in = new Scanner(System.in);
-        System.out.println("hola");
+        Scanner in = new Scanner(System.in);
+        System.out.println("hola2");
+        System.out.println("hola3");
     }
 }
